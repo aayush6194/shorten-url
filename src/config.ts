62e05e84f183a6d05b0199b2880457d396278e90
Aayush Shrestha <36089580+aayush6194@users.noreply.github.com>
2019-10-15 22:26:19 -0500

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@ import * as dotenv from 'dotenv';
 
 dotenv.config();
 //To Note: Environment Variables are Always type 'String'
-const Port =  Number(process.env.PORT) || 8080,
+const Port =  Number(process.env.PORT) || 80,
       MongoURI = process.env.MONGOURI || "mongodb://test:test12@ds345587.mlab.com:45587/proj-mng";
 
 export { Port, MongoURI };
